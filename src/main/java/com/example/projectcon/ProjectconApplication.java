cff0e469952227ffa5383102ad0712e42f1def5a
NONAE\goes__000 <ranate_sirinoy@cmu.ac.th>
2018-02-22 16:29:49 +0700

    First

---
+++ src/main/java/com/example/projectcon/ProjectconApplication.java
@@ -0,0 +1,12 @@
+package com.example.projectcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectconApplication.class, args);
+	}
+}
